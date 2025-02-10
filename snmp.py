99fc24502c2cdfc8d16a640a2de5f6c9a3d78cf7
ShelterHomePC <shelterxtech@gmail.com>
2025-02-11 05:15:41 +0700

    Fix cmd for windows

--- snmp.py
+++ snmp.py
@@ -3,10 +3,10 @@ import subprocess
 # Static values
 IP = "192.168.19.2"
 COMMUNITY = "zabbix_new"
-OID = "1.3.6.1.2.1.2.2.1.7.11"
+OID = ".1.3.6.1.2.1.2.2.1.7.11" # Eth 6
 
 def snmp_set(oid, value):
-    cmd = ["bin\SnmpSet.exe", "-v2c", "-c", COMMUNITY, IP, oid, "i", str(value)]
+    cmd = ["bin/SnmpSet.exe", f"-r:{IP}", "-v:2c", f"-c:{COMMUNITY}", f"-o:{oid}",  f"-val:{value}"]
     result = subprocess.run(cmd, capture_output=True, text=True)
     print("SNMP SET Result:", result.stdout)
 
