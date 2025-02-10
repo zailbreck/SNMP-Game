70a6e36f396977a9f5dda246c6c93011f10f741c
ShelterHomePC <shelterxtech@gmail.com>
2025-02-11 06:07:39 +0700

    feat: Add Dynamic Port Size

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-import random
+import random, os, subprocess
 import keyboard
-import os
 from colorama import Fore, Style, init
 from dataclasses import dataclass
 
+# Change Here
+IP = "ip_of_snmp_switch"
+COMMUNITY = "snmp_community"
+VERSION = "2c" # 1, 2c, 3
+BASE_OID = ".1.3.6.1.2.1.2.2.1.7." # Example
+ROWS = 2
+COLS = 24
+
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def snmp_set(oid, value):
+    cmd = ["bin/SnmpSet.exe", f"-r:{IP}", f"-v:{VERSION}", f"-c:{COMMUNITY}", f"-o:{oid}",  f"-val:{value}", f"-tp:int"]
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    print("SNMP SET Result:", result.stdout)
+
+def change_led(datas, state):
+    # State 1 for On - 2 for Off
+    for data in datas:
+        print(f"Set {data} to {"on" if state == 1 else "off"}")
+        #snmp_set(data, state)
+
+def generate_oid_matrix(base_oid, rows, cols):
+    if rows == 1:
+        matrix = [[str(base_oid + str(i)) for i in range(1, cols + 1)]]
+    elif rows == 2:
+        matrix = [
+            [str(base_oid + str(i)) for i in range(1, cols * 2, 2)],  # Ganjil
+            [str(base_oid + str(i)) for i in range(2, cols * 2 + 1, 2)]  # Genap
+        ]
+    else:
+        raise ValueError("Hanya mendukung rows=1 atau rows=2 untuk saat ini.")
+    
+    return matrix
 
 @dataclass
 class OIDMatrix:
@@ -74,7 +105,14 @@ class OIDMatrix:
         print("Snake length:", len(self.visited))
         
         snake_data = [self.matrix[i][j] for i, j in self.visited]
+        flat_oid_matrix = [item for sublist in oid_matrix.matrix for item in sublist]
+        avail_matrix = list(set(flat_oid_matrix) - set(snake_data))
+
         print("Snake Data:", snake_data)
+        change_led(snake_data, 1)
+
+        print("Available Data:", avail_matrix)
+        change_led(avail_matrix, 2)
         
         grid = [[Fore.RED + "+" + Style.RESET_ALL if (i, j) == self.food else Fore.BLUE + "-" + Style.RESET_ALL 
             for j in range(len(self.matrix[0]))] for i in range(len(self.matrix))]
@@ -96,20 +134,22 @@ class OIDMatrix:
         if self.food:
             print("Food is at:", self.matrix[self.food[0]][self.food[1]])
 
-base_oid = ".1.3.6.1.2.1.2.2.1.7."
-
-oid_matrix = OIDMatrix([
-    [str(base_oid + str(i)) for i in range(1, 24, 2)],
-    [str(base_oid + str(i)) for i in range(2, 25, 2)]
-], position=(0, 0), visited=[(0, 0)], food=None)
+oid_matrix = OIDMatrix(
+    generate_oid_matrix(BASE_OID, ROWS, COLS), 
+    position=(0, 0), 
+    visited=[(0, 0)], 
+    food=None
+)
 
 oid_matrix.spawn_food()
 
 def on_key_event(e):
     if oid_matrix.game_over and e.name == "r":
+        Style.RESET_ALL
         oid_matrix.reset_game()
         return
     elif oid_matrix.game_over and e.name == "x":
+        Style.RESET_ALL
         print("Exit!")
         os._exit(0)
     
